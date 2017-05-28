e0485af6ecc64002fe220ba276f7712c4965cc59
Igor Onischenko <optimum.flaminis@gmail.com>
2017-05-28 21:51:32 +0300

    file structure changed & video.html started

--- poker-web/gulpfile.js
+++ poker-web/gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
